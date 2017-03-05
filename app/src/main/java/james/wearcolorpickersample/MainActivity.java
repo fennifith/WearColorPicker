73b67ee0dbfc1bc78623c7e72fbeeb4be2620e10
James Fenn <18jafenn90@gmail.com>
2017-03-05 12:10:17 -0500

    Initial commit

---
+++ app/src/main/java/james/wearcolorpickersample/MainActivity.java
@@ -0,0 +1,25 @@
+package james.wearcolorpickersample;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //TODO: open color picker
+    }
+}
